12a4e73af69e1859b0773267efd0470a71020e29
Clemence Lelong <lelongclemence@gmail.com>
2013-10-21 15:34:13 +0200

    first commin

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Coursetconcours::Application.config.session_store :cookie_store, key: '_coursetconcours_session'
---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require superfish
+//= require jquery.flexslider-min
+//= require jquery.kwicks-1.5.1
+//= require jquery.easing.1.3
+//= require touchTouch.jquery
+// require_tree .
